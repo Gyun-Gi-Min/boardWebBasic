fcc4525a79ba43c77fae07daaab312439256cb1c
Gyun-Gi-Min <mingg95@gmail.com>
2021-12-03 16:03:25 +0900

    1203basic com

--- src/main/java/com/koreait/basic/board/BoardRegModServlet.java
+++ src/main/java/com/koreait/basic/board/BoardRegModServlet.java
@@ -1,7 +1,9 @@
 package com.koreait.basic.board;
 
 import com.koreait.basic.Utils;
+import com.koreait.basic.board.model.BoardDTO;
 import com.koreait.basic.board.model.BoardEntity;
+import com.koreait.basic.board.model.BoardVO;
 import com.koreait.basic.dao.BoardDAO;
 import com.koreait.basic.user.model.UserEntity;
 
@@ -18,6 +20,21 @@ public class BoardRegModServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         String title="글등록";
+
+
+        //수정 or 등록
+        int iboard = Utils.getParamaterInt(req,"iboard");
+
+        if(iboard > 0){ //수정
+            title="글수정";
+
+            BoardDTO dto =new BoardDTO();
+            dto.setIboard(iboard);
+
+            req.setAttribute("aaa",BoardDAO.selDetail(dto));
+
+        }
+
         Utils.displayView(title,"board/regmod",req,res);
     }
 
@@ -31,15 +48,24 @@ public class BoardRegModServlet extends HttpServlet {
             return;
         }
 
+        int iboard = Utils.getParamaterInt(req,"iboard");
+
         String title = req.getParameter("title");
         String ctnt = req.getParameter("ctnt");
 
+        int result = 0;
         BoardEntity entity = new BoardEntity();
         entity.setTitle(title);
         entity.setCtnt(ctnt);
         entity.setWriter(loginUserPk);
 
-        int result = BoardDAO.insBoard(entity);
+        if(iboard > 0){ //수정
+            entity.setIboard(iboard);
+            result = BoardDAO.updBoard(entity);
+        }else{//등록
+            result = BoardDAO.insBoardWithPk(entity);
+        }
+
         switch (result) {
             case 1:
                 if(entity.getIboard() != 0) {
--- src/main/java/com/koreait/basic/dao/BoardDAO.java
+++ src/main/java/com/koreait/basic/dao/BoardDAO.java
@@ -1,19 +1,45 @@
 package com.koreait.basic.dao;
 
+
 import com.koreait.basic.DbUtils;
-import com.koreait.basic.Utils;
 import com.koreait.basic.board.model.BoardDTO;
 import com.koreait.basic.board.model.BoardEntity;
 import com.koreait.basic.board.model.BoardVO;
-import com.mysql.cj.protocol.Resultset;
+import com.koreait.basic.user.model.UserEntity;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BoardDAO {
+    public static int insBoardWithPk(BoardEntity entity) {
+        int result = 0;
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        String sql = "INSERT INTO t_board(title, ctnt, writer)" +
+                "VALUES (?, ?, ?)";
+        try {
+            con = DbUtils.getCon();
+            ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, entity.getTitle());
+            ps.setString(2, entity.getCtnt());
+            ps.setInt(3, entity.getWriter());
+            result = ps.executeUpdate();
+            rs = ps.getGeneratedKeys();
+            if(rs.next()) {
+                int iboard = rs.getInt(1);
+                entity.setIboard(iboard);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            DbUtils.close(con, ps, rs);
+        }
+        return result;
+    }
+
+
     public static int insBoard(BoardEntity entity) {//title, ctnt,writer
         Connection con = null;
         PreparedStatement ps = null;
@@ -139,6 +165,27 @@ public class BoardDAO {
         } finally {
             DbUtils.close(con, ps);
         }
+
+    }
+
+    public static int updBoard(BoardEntity entity){
+        Connection con =null;
+        PreparedStatement ps= null;
+        String sql = "UPDATE t_board SET title =?, ctnt =?, mdt=now() WHERE iboard=? AND writer=?";
+
+        try {
+            con=DbUtils.getCon();
+            ps=con.prepareStatement(sql);
+            ps.setString(1,entity.getTitle());
+            ps.setString(2,entity.getCtnt());
+            ps.setInt(3,entity.getIboard());
+            ps.setInt(4,entity.getWriter());
+            return ps.executeUpdate();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            DbUtils.close(con,ps);
+        }return 0;
 
     }
         public static int delBoard(BoardEntity entity){
